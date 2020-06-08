4db848ab4e03c393097785c581fee1093da0f6e3
officiallysp777 <shane.pepperell@me.com>
2020-06-08 21:40:48 +0100

    shard + Creative Tab

---
+++ src/main/java/com/officiallysp/alchemicalexcellence/item/AEShardItem.java
@@ -0,0 +1,54 @@
+
+package com.officiallysp.alchemicalexcellence.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import com.officiallysp.alchemicalexcellence.itemgroup.AECreativetabItemGroup;
+import com.officiallysp.alchemicalexcellence.AlchemicalExcellenceModElements;
+
+@AlchemicalExcellenceModElements.ModElement.Tag
+public class AEShardItem extends AlchemicalExcellenceModElements.ModElement {
+	@ObjectHolder("alchemical_excellence:ae_shard")
+	public static final Item block = null;
+	public AEShardItem(AlchemicalExcellenceModElements instance) {
+		super(instance, 2);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(AECreativetabItemGroup.tab).maxStackSize(64));
+			setRegistryName("ae_shard");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+
+		@Override
+		@OnlyIn(Dist.CLIENT)
+		public boolean hasEffect(ItemStack itemstack) {
+			return true;
+		}
+	}
+}
