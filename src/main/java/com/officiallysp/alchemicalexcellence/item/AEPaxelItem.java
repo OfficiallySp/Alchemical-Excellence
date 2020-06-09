b2d105dfbdc2c9b1f547e8e114cc49b5675d7abd
officiallysp777 <shane.pepperell@me.com>
2020-06-09 02:01:19 +0100

    added fx for paxel

--- src/main/java/com/officiallysp/alchemicalexcellence/item/AEPaxelItem.java
+++ src/main/java/com/officiallysp/alchemicalexcellence/item/AEPaxelItem.java
@@ -2,13 +2,14 @@
 package com.officiallysp.alchemicalexcellence.item;
 
 import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
 
 import net.minecraft.world.World;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Item;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
@@ -19,6 +20,8 @@ import net.minecraft.block.BlockState;
 
 import java.util.List;
 
+import com.officiallysp.alchemicalexcellence.procedures.AEPaxelBlockDestroyedWithToolProcedure;
+import com.officiallysp.alchemicalexcellence.itemgroup.AECreativetabItemGroup;
 import com.officiallysp.alchemicalexcellence.AlchemicalExcellenceModElements;
 
 import com.google.common.collect.Multimap;
@@ -39,11 +42,36 @@ public class AEPaxelItem extends AlchemicalExcellenceModElements.ModElement {
 				super.addInformation(itemstack, world, list, flag);
 				list.add(new StringTextComponent("DEV USE ONLY"));
 			}
+
+			@Override
+			public boolean onBlockDestroyed(ItemStack itemstack, World world, BlockState bl, BlockPos pos, LivingEntity entity) {
+				boolean retval = super.onBlockDestroyed(itemstack, world, bl, pos, entity);
+				int x = pos.getX();
+				int y = pos.getY();
+				int z = pos.getZ();
+				{
+					java.util.HashMap<String, Object> $_dependencies = new java.util.HashMap<>();
+					$_dependencies.put("entity", entity);
+					$_dependencies.put("x", x);
+					$_dependencies.put("y", y);
+					$_dependencies.put("z", z);
+					$_dependencies.put("itemstack", itemstack);
+					$_dependencies.put("world", world);
+					AEPaxelBlockDestroyedWithToolProcedure.executeProcedure($_dependencies);
+				}
+				return retval;
+			}
+
+			@Override
+			@OnlyIn(Dist.CLIENT)
+			public boolean hasEffect(ItemStack itemstack) {
+				return true;
+			}
 		}.setRegistryName("ae_paxel"));
 	}
 	private static class ItemToolCustom extends Item {
 		protected ItemToolCustom() {
-			super(new Item.Properties().group(ItemGroup.TOOLS).maxDamage(2501));
+			super(new Item.Properties().group(AECreativetabItemGroup.tab).maxDamage(2501));
 		}
 
 		@Override
@@ -51,9 +79,9 @@ public class AEPaxelItem extends AlchemicalExcellenceModElements.ModElement {
 			Multimap<String, AttributeModifier> multimap = super.getAttributeModifiers(equipmentSlot);
 			if (equipmentSlot == EquipmentSlotType.MAINHAND) {
 				multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getName(),
-						new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Tool modifier", -0.5f, AttributeModifier.Operation.ADDITION));
+						new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Tool modifier", -2f, AttributeModifier.Operation.ADDITION));
 				multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getName(),
-						new AttributeModifier(ATTACK_SPEED_MODIFIER, "Tool modifier", -2, AttributeModifier.Operation.ADDITION));
+						new AttributeModifier(ATTACK_SPEED_MODIFIER, "Tool modifier", 1, AttributeModifier.Operation.ADDITION));
 			}
 			return multimap;
 		}
---
+++ src/main/java/com/officiallysp/alchemicalexcellence/procedures/AEPaxelBlockDestroyedWithToolProcedure.java
@@ -0,0 +1,86 @@
+package com.officiallysp.alchemicalexcellence.procedures;
+
+import net.minecraftforge.registries.ForgeRegistries;
+
+import net.minecraft.world.World;
+import net.minecraft.world.Explosion;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.item.ExperienceOrbEntity;
+import net.minecraft.entity.Entity;
+
+import com.officiallysp.alchemicalexcellence.AlchemicalExcellenceModElements;
+
+@AlchemicalExcellenceModElements.ModElement.Tag
+public class AEPaxelBlockDestroyedWithToolProcedure extends AlchemicalExcellenceModElements.ModElement {
+	public AEPaxelBlockDestroyedWithToolProcedure(AlchemicalExcellenceModElements instance) {
+		super(instance, 11);
+	}
+
+	public static void executeProcedure(java.util.HashMap<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			System.err.println("Failed to load dependency entity for procedure AEPaxelBlockDestroyedWithTool!");
+			return;
+		}
+		if (dependencies.get("x") == null) {
+			System.err.println("Failed to load dependency x for procedure AEPaxelBlockDestroyedWithTool!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			System.err.println("Failed to load dependency y for procedure AEPaxelBlockDestroyedWithTool!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			System.err.println("Failed to load dependency z for procedure AEPaxelBlockDestroyedWithTool!");
+			return;
+		}
+		if (dependencies.get("itemstack") == null) {
+			System.err.println("Failed to load dependency itemstack for procedure AEPaxelBlockDestroyedWithTool!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			System.err.println("Failed to load dependency world for procedure AEPaxelBlockDestroyedWithTool!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		int x = (int) dependencies.get("x");
+		int y = (int) dependencies.get("y");
+		int z = (int) dependencies.get("z");
+		ItemStack itemstack = (ItemStack) dependencies.get("itemstack");
+		World world = (World) dependencies.get("world");
+		if ((Math.random() < 0.9)) {
+			if (!world.isRemote) {
+				world.addEntity(new ExperienceOrbEntity(world, x, y, z, (int) 10));
+			}
+			if (entity instanceof PlayerEntity && !world.isRemote) {
+				((PlayerEntity) entity).sendStatusMessage(new StringTextComponent("XP Bonus Activated"), (true));
+			}
+			world.playSound((PlayerEntity) null, x, y, z,
+					(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.note_block.bell")),
+					SoundCategory.NEUTRAL, (float) 1, (float) 1);
+		}
+		if ((Math.random() < 0.9)) {
+			if (!world.isRemote) {
+				world.createExplosion(null, (int) x, (int) y, (int) z, (float) 1, Explosion.Mode.BREAK);
+			}
+			if (entity instanceof PlayerEntity && !world.isRemote) {
+				((PlayerEntity) entity).sendStatusMessage(new StringTextComponent("EarthQuake Activated"), (true));
+			}
+			world.playSound((PlayerEntity) null, x, y, z,
+					(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.note_block.bell")),
+					SoundCategory.NEUTRAL, (float) 1, (float) 1);
+		}
+		if ((Math.random() < 0.9)) {
+			((itemstack)).setDamage((int) 0);
+			if (entity instanceof PlayerEntity && !world.isRemote) {
+				((PlayerEntity) entity).sendStatusMessage(new StringTextComponent("Full Repair Activated"), (true));
+			}
+			world.playSound((PlayerEntity) null, x, y, z,
+					(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.note_block.bell")),
+					SoundCategory.NEUTRAL, (float) 1, (float) 1);
+		}
+	}
+}
